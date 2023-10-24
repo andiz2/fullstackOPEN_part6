30246d2be7176dff087a00c466385918ef11dc1e
andiz2 <codeandi@gmail.com>
2023-10-24 09:50:52 +0300

    Create notificationReducer.js
    redux toolkit

---
+++ 6b/de_retest/6_10/src/reducers/notificationReducer.js
@@ -0,0 +1,25 @@
+const notificationSlice = createSlice({
+    name: "notification",
+    initialState: null,
+    reducers: {
+      notify(state, action) {
+        return action.payload
+      },
+      reset(state, action) {
+        return null
+      }
+    }
+})
+
+export const {notify, reset} = notificationSlice.actions
+
+export const setNotification = (content, delay) => {
+    return (dispatch) => {
+      dispatch(notify(content))
+      setTimeout(() => {
+        dispatch(reset())
+      }, delay * 2000
+    }
+}
+
+export default notificationSlice.reducer
